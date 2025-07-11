226c2b396c72d2e21c1a3bac3f04363fa4582379
Meche221 <binaymeche22@gmail.com>
2025-07-11 15:47:16 +0545

    Learned how to add 1 in input

--- index.js
+++ index.js
@@ -3,5 +3,16 @@ let username;
 document.getElementById("submit-button").onclick = function(){
   username = document.getElementById("mytext").value;
   document.getElementById("mytext").value = '';
-  document.querySelector("h3").innerHTML = `Hello! and welcome ${username} in our webpage`;
+  document.querySelector("h3").textContent = `Hello! and welcome ${username} in our webpage`;
 }
+
+
+let age;
+
+document.getElementById("enter-button").onclick= function(){
+    age = document.getElementById("myage").value;
+    age = 1 + (Number(document.getElementById("myage").value));
+    document.getElementById("myage").value = "";
+    console.log(age);
+    document.querySelector("#age-result").innerHTML = `You age is ${age}`;
+  }
